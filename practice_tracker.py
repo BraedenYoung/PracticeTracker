169fb62f3b4262e3731f394415defd6e793fb091
Braeden Young <braeden.young@workiva.com>
2015-12-03 00:55:08 -0500

    PT : Update with coloured output and include a time cap to avoid issues from missing presses

--- practice_tracker.py
+++ practice_tracker.py
@@ -1,12 +1,13 @@
-
-import sys
 from datetime import datetime
+import logging
+import sys
 import time
 import urllib
 import urllib2
 
 from dateutil import tz
 from scapy.all import *
+from termcolor import colored
 
 
 MAGIC_FORM_URL = 'http://api.cloudstitch.com/braedenyoung/magic-form/datasources/sheet'
@@ -26,27 +27,31 @@ def main_loop():
 def arp_display(pkt):
     if pkt[ARP].op == 1: #who-has (request)
         if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-            print "ARP Probe from: " + pkt[ARP].hwsrc
+            print "ARP Probe from: ", colored(pkt[ARP].hwsrc, 'blue')
 
-            if pkt[ARP].hwsrc == '74:c2:46:18:2b:16': # Gatorade Button
-                print "Pressed!"
+            if pkt[ARP].hwsrc in ['a0:02:dc:e5:3b:d6', '74:c2:46:18:2b:16']: # Gatorade Button
                 record_event()
             else:
-                print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
+                print "ARP Probe from unknown device: ", colored(pkt[ARP].hwsrc, 'yellow')
 
 
 def record_event():
-    print 'Recording event'
 
     global currently_practicing, previous_time
 
-    current_time = datetime.utcnow()
+    print colored('Recording the event', 'green')
 
+    current_time = datetime.utcnow()
     time_difference = ''
 
     if currently_practicing:
-        time_difference = get_time_delta(current_time, previous_time)
-        print 'Practice Time : %s' % time_difference
+        try:
+            time_difference = get_time_delta(current_time, previous_time)
+        except MissedPressError:
+            print colored('ERROR!', 'red', attrs=['bold', 'blink'])
+            print colored('Button not pressed on previous day, default average used.', 'red')
+            time_difference = 20
+        print 'Practice Time : ', colored(time_difference, 'magenta')
         previous_time = None
     else:
         previous_time = current_time
@@ -61,23 +66,36 @@ def record_event():
 
 
 def get_time_delta(current, previous):
-    return strfdelta((current - previous),
-    '{minutes}') if previous else ''
+    try:
+        return strfdelta((current - previous),
+        '{minutes}') if previous else ''
+    except MissedPressError:
+        raise MissedPressError()
 
 
 def format_and_localize_time(time_value):
+    time_value = time_value.replace(tzinfo=tz.tzutc())
+    localized_time = time_value.astimezone(tz.tzlocal())
 
-        time_value = time_value.replace(tzinfo=tz.tzutc())
-        localized_time = time_value.astimezone(tz.tzlocal())
-
-        return localized_time.strftime("%Y-%m-%d %H:%M:%S")
+    return localized_time.strftime("%Y-%m-%d %H:%M:%S")
 
 
 def strfdelta(tdelta, fmt):
-    d = {"days": tdelta.days}
-    d["hours"], rem = divmod(tdelta.seconds, 3600)
-    d["minutes"], d["seconds"] = divmod(rem, 60)
+    d = {'days': tdelta.days}
+    if d['days']!= 0:
+        raise MissedPressError()
+
+    d['hours'], rem = divmod(tdelta.seconds, 3600)
+    d['minutes'], d['seconds'] = divmod(rem, 60)
+
+    if d['hours'] >= 1:
+        d['minutes'] += d['hours'] * 60
+
     return fmt.format(**d)
+
+
+class MissedPressError(Exception):
+     pass
 
 
 if __name__ == '__main__':
